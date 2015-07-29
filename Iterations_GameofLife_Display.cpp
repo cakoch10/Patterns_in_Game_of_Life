b6f18315337a1f20ae9f54af30a7158ee9035845
Caleb Koch <cakoch10@users.noreply.github.com>
2015-07-29 17:10:27 -0500

    Update Iterations_GameofLife_Display.cpp

--- Iterations_GameofLife_Display.cpp
+++ Iterations_GameofLife_Display.cpp
@@ -36,13 +36,8 @@ int counter=0;
 int main()
 {
 	system("MODE 1000,100");
-
-
 	int k=60;
 	vector< vector <int> > grid;
-	
-
-	
 	//	grid=newAutomaton();
 	//	outVec(grid);
 	int x=0;
@@ -53,19 +48,17 @@ int main()
 		outVec(grid);
 		while(!test(grid))
 		{
-	
 			while(x<229)
 			{
-		
-			grid=updateVec(grid);
-			grid=checkVec(grid);
-			x++;
+				grid=updateVec(grid);
+				grid=checkVec(grid);
+				x++;
 			}
+			
 			outVec2(grid);
 			grid=updateVec(grid);
 			grid=checkVec(grid);
 			system("PAUSE");
-
 		}
 		out2file();
 		grid.clear();
@@ -81,7 +74,6 @@ int main()
 	cout<< grid[0][0]<<grid[0][1];
 	cin.get();
 	*/
-	
 	return 0;	
 }
 
@@ -106,7 +98,6 @@ void outVec(vector< vector <int> > grid)
 				cout<<" ";
 			if(counter2+1==size)
 				cout<<endl;
-							
 			counter2++;
 		}
 		counter2=0;
@@ -114,7 +105,6 @@ void outVec(vector< vector <int> > grid)
 	}
 	cout<<endl<<endl;
 	cout<<"T= "<<num_gens<<endl;
-
 }
 
 void outVec2(vector< vector <int> > grid)
@@ -146,7 +136,6 @@ void outVec2(vector< vector <int> > grid)
 	generations<<"\n";
 	generations<<"T= "<<num_gens<<endl;
 	generations.close();
-
 }
 
 
@@ -157,10 +146,7 @@ vector < vector<int> > updateVec(vector< vector <int> > grid)
 	if(grid.size()!=grid[0].size())
 		cout<<"Error. Grid is no longer a square.";
 	int size=grid.size();
-	
 	vector< vector<int> > newgrid(size, vector<int>(size));//create an updated automaton
-	
-	
 	int c1=1;
 	int c2=1;
 	while(c1<size-1)
@@ -171,21 +157,14 @@ vector < vector<int> > updateVec(vector< vector <int> > grid)
 			
 			//apply the transition rules
 			if(grid[c1][c2]==0&&sum_neighborhood==3)
-			{
 				newgrid[c1][c2]=1;
-			}
+			
 			else if(grid[c1][c2]==0&&sum_neighborhood!=3)
-			{
 				newgrid[c1][c2]=0;
-			}
 			else if(grid[c1][c2]==1&&(sum_neighborhood==2||sum_neighborhood==3))
-			{
 				newgrid[c1][c2]=1;
-			}
 			else if(grid[c1][c2]==1&&(sum_neighborhood!=2&&sum_neighborhood!=3))
-			{
 				newgrid[c1][c2]=0;
-			}
 			c2++;
 		}
 		c2=1;
@@ -211,8 +190,6 @@ bool test(vector< vector <int> > grid)
 		}
 	}
 	*/
-	
-	
 	if(prelim==0)
 	{
 		prevgrid=grid;
@@ -250,13 +227,13 @@ void out2file()
 {
 	if(val_n%2==0)
 	{
-	ofstream generations("generationseven.txt", ios_base::out | ios_base::app);
-	generations<<num_gens<<endl;
-	generations.close();
+		ofstream generations("generationseven.txt", ios_base::out | ios_base::app);
+		generations<<num_gens<<endl;
+		generations.close();
 	
-	ofstream size_init("value_of_neven.txt", ios_base::out | ios_base::app);
-	size_init<<val_n<<endl;
-	size_init.close();
+		ofstream size_init("value_of_neven.txt", ios_base::out | ios_base::app);
+		size_init<<val_n<<endl;
+		size_init.close();
 	}
 	//reset values
 	prev2grid.clear();
@@ -281,33 +258,24 @@ vector< vector <int> > checkVec(vector< vector <int> > grid)
 	int sumElementsOuter=0;
 	
 	for(vector<int>::iterator j=grid[0].begin();j!=grid[0].end();++j)
-    	sumElementsOuter += *j;
-	
+    		sumElementsOuter += *j;
 	for(vector<int>::iterator k=grid[size-1].begin();k!=grid[size-1].end();++k)
-    	sumElementsOuter += *k;
-	
+    		sumElementsOuter += *k;
 	for(int m=1; m<size-1; m++)
-	{
 		sumElementsOuter=grid[m][0]+grid[m][size-1]+sumElementsOuter;
-	}
 	
 	//add up the sum of the states in the layer right before the outer
 	int sumElementsIn=0;
 	
 	for(vector<int>::iterator x=grid[1].begin();x!=grid[1].end();++x)
-    	sumElementsIn += *x;
+    		sumElementsIn += *x;
 	
 	for(vector<int>::iterator y=grid[size-2].begin();y!=grid[size-2].end();++y)
-    	sumElementsIn += *y;
+    		sumElementsIn += *y;
 	
 	for(int m=2; m<size-2; m++)
-	{
 		sumElementsIn=grid[m][1]+grid[m][size-2]+sumElementsIn;
-	}
-	
-	
-	
-	
+
 			if(sumElementsOuter>0)
 			{
 				//gird needs to be updated to have two new outer layers
@@ -351,6 +319,7 @@ vector< vector <int> > checkVec(vector< vector <int> > grid)
 				}
 			
 			}
+			
 			else if(sumElementsIn>0)
 			{
 				//gird needs to be updated to have one new outer layers
@@ -386,9 +355,7 @@ vector< vector <int> > checkVec(vector< vector <int> > grid)
 				}
 			
 			}
-			
 	return grid;
-	
 }
 
 
@@ -441,7 +408,6 @@ vector< vector<int> > newAutomaton()
 	//initialize all the values to 0 at first
 	while(count2<sideLen)
 	{
-	
 		while(count3<sideLen)
 		{
 			grid[count2][count3]=0;
@@ -467,18 +433,6 @@ vector< vector<int> > newAutomaton()
 	}
 	return grid;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -515,18 +469,6 @@ vector< vector<int> > newAutomaton()
 		counter1++;
 	}
 	
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
